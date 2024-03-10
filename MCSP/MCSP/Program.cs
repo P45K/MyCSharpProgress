d2a850fcb572afa7d0e530eed54cc032b9127ee7
P45K <160267533+P45K@users.noreply.github.com>
2024-03-10 13:53:39 +0100

    Dzień 1 - Skrócone wyrażenia warunkowe

--- MCSP/MCSP/Program.cs
+++ MCSP/MCSP/Program.cs
@@ -1,29 +1,43 @@
-﻿
-Console.WriteLine("Ile masz lat?");
-int wiek = int.Parse(Console.ReadLine());
+﻿//// Instrukcje warunkowe - If, ELSE IF, ELSE
 
-if (wiek >= 18)
-{
-    Console.WriteLine("Jesteś pełnoletni" + " Masz: " + wiek + " lat");
-}
-else
-{
-    Console.WriteLine("Nie jesteś pełnoletni");
-    Console.WriteLine("Przykro mi");
-}
+//Console.WriteLine("Ile masz lat?");
+//int wiek = int.Parse(Console.ReadLine());
 
+//if (wiek >= 18)
+//{
+//    Console.WriteLine("Jesteś pełnoletni" + " Masz: " + wiek + " lat");
+//}
+//else
+//{
+//    Console.WriteLine("Nie jesteś pełnoletni");
+//    Console.WriteLine("Przykro mi");
+//}
+
+//Console.WriteLine("Podaj liczbę:");
+//int number = int.Parse(Console.ReadLine());
+
+//if (number > 0)
+//{
+//    Console.WriteLine("Liczba większa od 0");
+//}
+//else if (number < 0)
+//{
+//    Console.WriteLine("liczba jest mniejsza od zera");
+//}
+//else 
+//{
+//    Console.WriteLine("Liczba równa 0");
+//}
+
+//==============================================================//
+
+// skrócony wyrażenia warunkowe - robi to samo co if{} i else{}
+// operator trójwarunkowy
 Console.WriteLine("Podaj liczbę:");
-int number = int.Parse(Console.ReadLine());
+int num = int.Parse(Console.ReadLine());
 
-if (number > 0)
-{
-    Console.WriteLine("Liczba większa od 0");
-}
-else if (number < 0)
-{
-    Console.WriteLine("liczba jest mniejsza od zera");
-}
-else 
-{
-    Console.WriteLine("Liczba równa 0");
-}
+string result = num % 2 == 0 ? "Parzysta" : "Nieparzysta";
+Console.WriteLine(result);
+
+//==============================================================//
+
