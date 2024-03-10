ddcf88135ac36dffc79ae764a0a24772c90d938f
P45K <160267533+P45K@users.noreply.github.com>
2024-03-10 13:44:47 +0100

    Dzień 1 - Instrukcje warunkowe (if, else if, else)

--- MCSP/MCSP/Program.cs
+++ MCSP/MCSP/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("H E L L O!");
+﻿
+Console.WriteLine("Ile masz lat?");
+int wiek = int.Parse(Console.ReadLine());
+
+if (wiek >= 18)
+{
+    Console.WriteLine("Jesteś pełnoletni" + " Masz: " + wiek + " lat");
+}
+else
+{
+    Console.WriteLine("Nie jesteś pełnoletni");
+    Console.WriteLine("Przykro mi");
+}
+
+Console.WriteLine("Podaj liczbę:");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 0)
+{
+    Console.WriteLine("Liczba większa od 0");
+}
+else if (number < 0)
+{
+    Console.WriteLine("liczba jest mniejsza od zera");
+}
+else 
+{
+    Console.WriteLine("Liczba równa 0");
+}
