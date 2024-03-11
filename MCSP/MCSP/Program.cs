fa7d5465c3f9b5e6536548096cdcf01ae7c5e91b
P45K <160267533+P45K@users.noreply.github.com>
2024-03-11 12:54:38 +0100

    Dzień 1 i 2 - praktyka - średnia arytmetyczna

--- MCSP/MCSP/Program.cs
+++ MCSP/MCSP/Program.cs
@@ -1,210 +1,25 @@
-﻿//// INSTRUKCJE WARUNKOWE - If, ELSE IF, ELSE
+﻿//==============================================================//
 
-//Console.WriteLine("Ile masz lat?");
-//int wiek = int.Parse(Console.ReadLine());
+// DZIEŃ 1 i 2 - PRAKTYKA
+// ŚREDNIA ARYTMETYCZNA
 
-//if (wiek >= 18)
-//{
-//    Console.WriteLine("Jesteś pełnoletni" + " Masz: " + wiek + " lat");
-//}
-//else
-//{
-//    Console.WriteLine("Nie jesteś pełnoletni");
-//    Console.WriteLine("Przykro mi");
-//}
+float a;    // przechowywane wartości od użytkownika (liczby wpisuywane przez użytkownika)    
+float sum = 0;  // suma podanych liczb, aż program nie napotka 0 (gdy użytkownik wpisze 0);
+int i = 0;  // ile liczb podał użytkownik
 
-//Console.WriteLine("Podaj liczbę:");
-//int number = int.Parse(Console.ReadLine());
-
-//if (number > 0)
-//{
-//    Console.WriteLine("Liczba większa od 0");
-//}
-//else if (number < 0)
-//{
-//    Console.WriteLine("liczba jest mniejsza od zera");
-//}
-//else 
-//{
-//    Console.WriteLine("Liczba równa 0");
-//}
-
-////==============================================================//
-
-//// SKRÓCONE WYRAŻENIA WARUNKOWE - robi to samo co if{} i else{}
-//// OPERATOR TRÓJWARUNKOWY
-
-//Console.WriteLine("Podaj liczbę:");
-//int num = int.Parse(Console.ReadLine());
-
-//string result = num % 2 == 0 ? "Parzysta" : "Nieparzysta";
-//Console.WriteLine(result);
-
-////==============================================================//
-
-//// INSTRUKCJA WYBORU - SWITCH
-
-//Console.WriteLine("Podaj dzień tygodnia (liczbowo od 1 do 7)");
-//int dniTygodnia = int.Parse(Console.ReadLine());
-
-//switch (dniTygodnia)
-//{
-//    case 1: // "case" zawsze sprawdza == 
-//        Console.WriteLine("Poniedziałek");
-//        break;
-//    case 2:
-//        Console.WriteLine("Wtorek");
-//        break;
-//    case 3:
-//        Console.WriteLine("Środa");
-//        break;
-//    case 4:
-//        Console.WriteLine("Czwartek");
-//        break;
-//    case 5:
-//        Console.WriteLine("Piątek");
-//        break;
-//    case 6:
-//    case 7:
-//        Console.WriteLine("Weekend");
-//        break;
-//    default:
-//        Console.WriteLine("Nie ma takiego dnia tygodnia!");
-//        break;
-//}
-
-////
-//Console.WriteLine("Podaj kolor samochodu:");
-//string kolorSamochodu = Console.ReadLine(); 
-//string marka; 
-//switch (kolorSamochodu) 
-//{ 
-//    case "czerwony": 
-//        marka = "Ferrari"; 
-//        break; 
-//    case "niebieski": 
-//        marka = "Ford"; 
-//        break; 
-//    case "czarny": 
-//        marka = "Nissan"; 
-//        break;
-//    case "zielony":
-//        marka = "Toyota";
-//        break;
-//    default: 
-//        marka = "Nieznana"; 
-//        break; 
-//}
-
-//Console.WriteLine("Marka samochodu o kolorze {0} to {1}.", kolorSamochodu, marka);
-
-////==============================================================//
-
-//// PĘTLA - WHILE
-
-//Console.Write("Podaj liczbę:");
-//int a = int.Parse(Console.ReadLine());
-
-//int i = 1; // wg konwencji od literki "i" zaczynają sie nazwy zmiennych pomocniczych
-
-//while (i <= a) 
-//{
-//    Console.WriteLine(i);
-//    i++;
-//}
-
-
-////==============================================================//
-
-//// PĘTLA - DO WHILE
-//// - różnica między WHILE a DO WHILE jest taka, że jeśli warunek nie zostanie spełniony, to pętla WHILE nie uruchomi się a DO WHILE wykona się przynajmniej jeden raz
-
-//Console.Write("Podaj liczbę: ");
-
-//int b = int.Parse(Console.ReadLine());
-
-//int j = 1; // wg konwencji od literki "i" zaczynają sie nazwy zmiennych pomocniczych
-
-//Console.WriteLine("While");
-//while (j <= b)
-//{
-//    Console.WriteLine(j);
-//    j++;
-//}
-
-//Console.WriteLine("Do While");
-//j = 1;
-
-//do
-//{
-//    Console.WriteLine(j);
-//    j++;
-//} while (j <= b);
-
-//// === PROGRAMIK PODAJ WIEK > 0, < 100 === //
-
-//int wiek = 0;   // jeśli użytkownik wprowadzi wiek mniejszy od zera (np -1) i większy od 100 (np 101), to program wykonapętlę ponownie.
-
-//do
-//{
-//    Console.WriteLine("Wprowadź wiek większy od zera, ale mniejszy od 100:");
-//    wiek = Convert.ToInt32(Console.ReadLine());
-//} while (wiek <= 0 || wiek >= 100);
-
-
-////==============================================================//
-
-
-//// PĘTLA - FOR
-
-//Console.WriteLine("Podaj liczbę: ");
-//int a = int.Parse(Console.ReadLine());
-//int b = int.Parse(Console.ReadLine());
-
-//for ( int i = 1; i <= a; i++) // gdzie: (i = 1;) - delklaracja zmiennej pomocniczej, (i <= a;) - warunek wykonania petli, (i++) - instrukcja, która zostanie wykonana na końcu petli aż do wykonania warunku pętli
-//{
-//    Console.WriteLine(i);
-//}
-
-//// PĘTLA - FOR EACH
-//// FOR EACH - TO PETLA OBIEKTOWA i NIE DZIAŁA W KONTEKŚCIE DLA POJEDYŃCZEJ LICZBY.
-
-//Console.WriteLine("Podaj tekst: ");
-//string text = Console.ReadLine();
-
-//foreach (char litera in text)
-//{
-//    Console.WriteLine(litera);
-//}
-
-//==============================================================//
-
-// INSTRUKCJE SKOKU - BREAK i CONTINUE
-
-//for (int i = 0; ; i++)  // pętla wykona sie do 20 i zakończy działanie
-//{
-//    if (i > 20) 
-//    {
-//        break;
-//    }
-//    if (i % 2 == 1) // warunek sprawdza czy liczba jest nieparzysta, pętla wyświetli tylko wartości parzyste od 0 do 20
-//    {
-//        continue;
-//    }
-//    Console.WriteLine(i);
-}
-
-//==============================================================//
-
-// INSTRUKCJA SKOKU - GOTO
-
-    int i = 0;
-hello:  // etykieta "hello:"
-    Console.WriteLine("Hello");
+do
+{
+    Console.Write("Podaj liczbę: ");
+    a = float.Parse(Console.ReadLine());
+    if (a == 0) // ta pętla odejmie "0" od ilości podanych liczb
+    {
+        break;  // instrukcja skoku przerywająca pętlę
+    }
+    sum += a;
     i++;
+} while (a != 0);   // warunek kontynuacji pętli (a musi być różne od 0, żeby pętla się wykonywała)
 
-    if(i < 10)
-        goto hello; // program wraca do lini z etykietą "hello:"
+Console.WriteLine("Średnia arytmetyczna to: {0}", sum / i);
 
 
 //==============================================================//
