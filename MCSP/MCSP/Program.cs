28cf8de3b964f97f23197539b911b1043939097a
P45K <160267533+P45K@users.noreply.github.com>
2024-03-12 11:28:39 +0100

    Dzień 4 - FUNKCJE - zwracanie wartości

--- MCSP/MCSP/Program.cs
+++ MCSP/MCSP/Program.cs
@@ -22,19 +22,41 @@
 
 //==============================================================//
 
-// FUNKCJE - ARGUMENTY
+//    // FUNKCJE - ARGUMENTY
 
-MojaFunkcja(" H E L L O   W O R L D! ", 5);
+//MojaFunkcja(" H E L L O   W O R L D! ", 5);
 
-static void MojaFunkcja(string tekst, int ileRazy)  // funkcja wieloargumentowa - wyświetli 5 razy napis " H E L L O   W O R L D!"
-{
-    for (int i = 0; i < ileRazy; i++)
-    {
-        Console.WriteLine(tekst);
-    }
-}
+//static void MojaFunkcja(string tekst, int ileRazy)  // funkcja wieloargumentowa - wyświetli 5 razy napis " H E L L O   W O R L D!"
+//{
+//    for (int i = 0; i < ileRazy; i++)
+//    {
+//        Console.WriteLine(tekst);
+//    }
+//}
 
 //==============================================================//
 
+// FUNKCJE - ZWRACANIE WARTOŚCI
+
+
+int res = FunkcjaAdd(5, 10);
+Console.WriteLine(res);
+
+static int FunkcjaAdd(int a, int b) // funkcja typu int 
+{
+    int sum = a + b;
+    return sum; // return kończy, przerywa działanie funkcji i powinno być zawsze na końcu funkcji
+}
+
+    // prostsza wersja
+
+    //  Console.WriteLine(FunkcjaAdd2(5, 10));
+
+    //  static int FunkcjaAdd2(int c, int d)
+    //  {
+    //      return c + d;
+    //  }
+
+//==============================================================//
 
 
