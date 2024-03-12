f64a38d8b34cc518c108ccb24a11299ac6060443
P45K <160267533+P45K@users.noreply.github.com>
2024-03-12 10:57:13 +0100

    Dzień 4 - FUNKCJE - funkcje z argumentami

--- MCSP/MCSP/Program.cs
+++ MCSP/MCSP/Program.cs
@@ -1,25 +1,40 @@
-﻿//==============================================================//
+﻿////==============================================================//
 
-//  === FUNKCJE ===
+////  === FUNKCJE ===
 
-    // FUNKCJE BEZARGUMENTOWE
+//    // FUNKCJE BEZARGUMENTOWE
 
-    /* - FUNKCE TWORZYMY BEZPOŚREDNIO W KLASIE
-       - FUNKCJE MUSZĄ BYĆ ZAKOŃCZONE NAWIASAMI ()
-       - W NAWIASACH () ZNAJDUJĄ SIĘ ARGUMENTY FUNKCJI
-       - FUNKCJA POSIADA CIAŁO W NAWIASACH {}
-       - FUNKCJĘ MOŻNA WYWOŁAĆ PO JEJ NAZWIE WIELE RAZY W RÓŻNYCH MIEJSCACH */
+//    /* - FUNKCE TWORZYMY BEZPOŚREDNIO W KLASIE
+//       - FUNKCJE MUSZĄ BYĆ ZAKOŃCZONE NAWIASAMI ()
+//       - W NAWIASACH () ZNAJDUJĄ SIĘ ARGUMENTY FUNKCJI
+//       - FUNKCJA POSIADA CIAŁO W NAWIASACH {}
+//       - FUNKCJĘ MOŻNA WYWOŁAĆ PO JEJ NAZWIE WIELE RAZY W RÓŻNYCH MIEJSCACH */
 
-HelloWorld();   // wywołanie funkcji
-HelloWorld();
-HelloWorld();
-HelloWorld();
+//HelloWorld();   // wywołanie funkcji
+//HelloWorld();
+//HelloWorld();
+//HelloWorld();
 
-static void HelloWorld()    // funkcja bezargumentowa 
+//static void HelloWorld()    // funkcja bezargumentowa 
+//{
+//    Console.WriteLine("H E L L O   W O R L D!");
+//}
+
+//==============================================================//
+
+// FUNKCJE - ARGUMENTY
+
+MojaFunkcja(" H E L L O   W O R L D! ", 5);
+
+static void MojaFunkcja(string tekst, int ileRazy)  // funkcja wieloargumentowa - wyświetli 5 razy napis " H E L L O   W O R L D!"
 {
-    Console.WriteLine("H E L L O   W O R L D!");
+    for (int i = 0; i < ileRazy; i++)
+    {
+        Console.WriteLine(tekst);
+    }
 }
 
-//==============================================================// 
+//==============================================================//
+
 
 
